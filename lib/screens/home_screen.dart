78d410d652af8c3194fa7dba2038b1f075386bbb
Nicolas Pádua <120571274+NicolasPFerraz@users.noreply.github.com>
2026-09-08 21:42:25 +0000

    fix(interface): restaura alinhamento à direita dos cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -822,12 +822,13 @@ class _StatusCard extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(width: 10),
-                    Flexible(
+                    Expanded(
                       flex: 2,
                       child: Text(
                         value,
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.right,
                         style: const TextStyle(
                           color: AppColors.white,
                           fontSize: 14,
